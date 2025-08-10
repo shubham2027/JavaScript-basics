f8a85e1061002f75575a3e4e06cdadee63727fc5
SHUBHAM KUMAR SINGH <149414603+shubham2027@users.noreply.github.com>
2025-08-10 10:43:50 +0000

    comparison file

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+
+console.log("2" > 1); //typecasting is done here automatically
+console.log("02" > 1); 
