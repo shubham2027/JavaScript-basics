8e3da94b6765e33c977d2b8ecb4c90e0858a356d
SHUBHAM KUMAR SINGH <149414603+shubham2027@users.noreply.github.com>
2025-08-10 14:44:40 +0000

    comparison update 2

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -5,5 +5,23 @@
 // console.log(2 <= 1);
 // console.log(2 >= 1);
 
-console.log("2" > 1); //typecasting is done here automatically
-console.log("02" > 1); 
+// console.log("2" > 1); //typecasting is done here automatically
+// console.log("02" > 1); 
+
+
+// Because before executing the null is changes to zero at backend 
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined > 0); //false 
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0); //false
+
+
+
+// === strictly equal to (no changes at backend)
+
+
+
+
